837d46c3ca3a9d0850abe06d6f2a695a3c3af27e
zpc61114 <89431105+zpc61114@users.noreply.github.com>
2022-01-10 19:39:00 -0500

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,17 @@
+//This is an orientation assignment program
+//January 10, 2022
+//CSC-234-0901
+//Zachary Castillo
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello my name is Zachary Castillo." << endl;
+    cout << "I am currently studying Advanced python as well as this advanced C++ course." << endl;
+    cout << "In my free time I enjoy spending time with my kids and working out." << endl;
+
+    return 0;
+}
